c0fd889e532e2c17b46cc44b655d128c8c112b4e
Pablo Verdúguez <91903837+PabloVerduguez@users.noreply.github.com>
2024-09-30 11:19:09 +0200

    Refactorizar ServiceRepartidor y agregar métodos para buscar, guardar y eliminar repartidores

---
+++ src/main/java/com/IsoII/DeliveringSolutions/persistencia/RepartidorDAO.java
@@ -0,0 +1,9 @@
+package com.IsoII.DeliveringSolutions.persistencia;
+
+import org.springframework.stereotype.Repository;
+import com.IsoII.DeliveringSolutions.dominio.entidades.Repartidor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface RepartidorDAO extends JpaRepository<Repartidor, Long> {
+}
