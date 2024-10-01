0bd4d711cf7223e0d0200e8ab226c22bef0350f0
marcowork2707 <marco.munoz@alu.uclm.es>
2024-10-01 10:22:29 +0200

    JPA implementado en Pago.java

--- src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/Pago.java
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/Pago.java
@@ -3,6 +3,15 @@ package com.IsoII.DeliveringSolutions.dominio.entidades;
 import java.sql.Date;
 import java.util.UUID;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 /**
  * Representa un pago asociado a un pedido, incluyendo el método de pago, identificador de transacción y fecha de transacción.
  * 
@@ -12,11 +21,23 @@ import java.util.UUID;
  * @author Marco Muñoz García
  * @version 1.0
  */
+
+ @Entity
 public class Pago {
 
+    @ManyToOne
+    @JoinColumn (name = "idPedido", nullable = false)
     private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn (name = "tipoPago", nullable = false)
     private MetodoPago tipo;
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private UUID idTransaccion;
+
+    @Column (name = "fechaTransaccion", nullable = false)
     private Date fechaTransaccion;
 
     /**
