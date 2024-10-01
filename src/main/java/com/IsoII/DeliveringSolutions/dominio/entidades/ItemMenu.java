8652f15093f09a6dd48fb18f8bd6d23f21ef016b
marcowork2707 <marco.munoz@alu.uclm.es>
2024-10-01 10:41:21 +0200

    JPA implementado en ItemMenu.java

--- src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/ItemMenu.java
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/ItemMenu.java
@@ -1,5 +1,11 @@
 package com.IsoII.DeliveringSolutions.dominio.entidades;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 /**
  * Representa un ítem del menú en un restaurante, incluyendo su tipo, nombre y precio.
  * 
@@ -9,11 +15,27 @@ package com.IsoII.DeliveringSolutions.dominio.entidades;
  * @author Marco Muñoz García
  * @version 1.0
  */
+
+ @Entity
 public class ItemMenu {
 
+    @Id
+    private int idMenu;
+
+    @ManyToOne
+    @JoinColumn (name = "menu_id", nullable = false)
     private TipoItemMenu tipo;
+
+    @ManyToOne
+    @JoinColumn (name = "menu_id", nullable = false)
+    private CartaMenu menu_id;
+
+    @Column (name = "nombre", nullable = false)
     private String nombre;
+
+    @Column (name = "precio", nullable = false)
     private double precio;
+    
 
     /**
      * Constructor para crear un ítem del menú con un tipo, nombre y precio específicos.
@@ -22,10 +44,12 @@ public class ItemMenu {
      * @param nombre El nombre del ítem del menú.
      * @param precio El precio del ítem del menú.
      */
-    public ItemMenu(TipoItemMenu tipo, String nombre, double precio) {
+    public ItemMenu(int idMenu, TipoItemMenu tipo, String nombre, double precio, CartaMenu menu_id) {
+        this.idMenu = idMenu;
         this.tipo = tipo;
         this.nombre = nombre;
         this.precio = precio;
+        this.menu_id = menu_id;
     }
 
     /**
@@ -80,6 +104,42 @@ public class ItemMenu {
      */
     public void setPrecio(double precio) {
         this.precio = precio;
+    }
+
+    /**
+     * Obtiene el identificador
+     * @return El identificador del ítem del menú.
+     */
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    /**
+     * Establece el identificador del ítem del menú.
+     * @param idMenu El identificador del ítem del menú a establecer.
+     */
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    /**
+     * Obtiene el menú al que pertenece el ítem.
+     * @return El menú al que pertenece el ítem.
+     */
+
+    public CartaMenu getMenu_id() {
+        return menu_id;
+    }
+
+    /**
+     * Establece el menú al que pertenece el ítem.
+     * @param menu_id El menú al que pertenece el ítem.
+     */
+
+    public void setMenu_id(CartaMenu menu_id) {
+        this.menu_id = menu_id;
     }
 
     /**
