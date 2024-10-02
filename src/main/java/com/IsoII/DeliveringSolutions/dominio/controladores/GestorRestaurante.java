43adc680f457cc609a2881ea6039f5b790a425ae
lopezmIsmael <ismael.lopez6@alu.uclm.es>
2024-10-02 21:49:31 +0200

    Refactorize GestorRestaurante.java to update restaurant registration functionality

--- src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorRestaurante.java
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorRestaurante.java
@@ -1,18 +1,12 @@
 package com.IsoII.DeliveringSolutions.dominio.controladores;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
@@ -22,41 +16,37 @@ import com.IsoII.DeliveringSolutions.persistencia.RestauranteDAO;
 @Controller
 @RequestMapping("/restaurantes")
 public class GestorRestaurante {
-    RedirectAttributes redirectAttributes = new RedirectAttributesModelMap() {
+    RedirectAttributes redirectAttributes = new RedirectAttributesModelMap();
 
-        @Autowired
-        private RestauranteDAO restauranteDAO;
+    @Autowired
+    private RestauranteDAO restauranteDAO;
 
-        @GetMapping("/findAll")
-        @ResponseBody
-        public List<Restaurante> findAll() {
-            return restauranteDAO.findAll();
+    @GetMapping("/findAll")
+    @ResponseBody
+    public List<Restaurante> findAll() {
+        return restauranteDAO.findAll();
+    }
+
+    @GetMapping("/register")
+    public String mostrarFormularioRegistro() {
+        return "Pruebas-RegisterRestaurante"; // Nombre del archivo HTML sin la extensión
+    }
+
+    @GetMapping("/findById/{id}")
+    @ResponseBody
+    public Restaurante findById(@PathVariable String id) {
+        return restauranteDAO.findById(id).orElse(null);
+    }
+
+    @PostMapping("/registrarRestaurante")
+    public ResponseEntity<Restaurante> registrarRestaurante(@ModelAttribute Restaurante restaurante) {
+        System.out.println("Restaurante recibido: " + restaurante.toString());
+        if (restaurante.getPass() == null || restaurante.getPass().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        @GetMapping("/register")
-        public String mostrarFormularioRegistro() {
-            return "Pruebas-RegisterRestaurante"; // Nombre del archivo HTML sin la extensión
-        }
-
-        @GetMapping("/findById/{id}")
-        @ResponseBody
-        public Restaurante findById(@PathVariable String id) {
-            return restauranteDAO.findById(id).orElse(null);
-        }
-
-
-        @PostMapping("/registrarRestaurante")
-        public ResponseEntity<Restaurante> registrarRestaurante(@ModelAttribute Restaurante restaurante) {
-            System.out.println("Restaurante recibido: " + restaurante.toString());
-            if (restaurante.getPass() == null || restaurante.getPass().isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-            Restaurante restauranteRegistrado = restauranteDAO.save(restaurante);
-            System.out.println("Restaurante registrado: " + restauranteRegistrado);
-            return new ResponseEntity<>(restauranteRegistrado, HttpStatus.CREATED);
-        }
-        
-        
-    };
+        Restaurante restauranteRegistrado = restauranteDAO.save(restaurante);
+        System.out.println("Restaurante registrado: " + restauranteRegistrado);
+        return new ResponseEntity<>(restauranteRegistrado, HttpStatus.CREATED);
+    }
 
 }
