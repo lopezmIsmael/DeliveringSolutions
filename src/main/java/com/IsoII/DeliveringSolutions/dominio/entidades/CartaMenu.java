3d0b01afed70e6f64c0851e99351af3074f7957d
Pablo Verdúguez <91903837+PabloVerduguez@users.noreply.github.com>
2024-10-18 22:35:59 +0200

    Refactorizar entidades CartaMenu e ItemMenu
    Se han refactorizado las entidades CartaMenu e ItemMenu para mejorar su estructura y funcionalidad. Se han agregado anotaciones de persistencia y se han definido las relaciones con otras entidades. Además, se han añadido constructores y métodos getter y setter para facilitar el acceso a los atributos de las entidades.

---
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/CartaMenu.java
@@ -0,0 +1,64 @@
+package com.IsoII.DeliveringSolutions.dominio.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Column;
+
+/**
+ * Representa un usuario genérico en el sistema con un id, nombre.
+ * 
+ * @autor Pablo Verdúguez Gervaso
+ * @version 1.0
+ */
+@Entity
+public class CartaMenu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCarta;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "idRestaurante", referencedColumnName = "cif")
+    Restaurante restaurante;
+
+    public CartaMenu() {
+    }
+
+    public CartaMenu(int idCarta, String nombre, Restaurante restaurante) {
+        this.idCarta = idCarta;
+        this.nombre = nombre;
+        this.restaurante = restaurante;
+    }
+
+    public int getIdCarta() {
+        return idCarta;
+    }
+
+    public void setIdCarta(int idCarta) {
+        this.idCarta = idCarta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+    
+}
