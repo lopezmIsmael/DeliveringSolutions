019b3ccbff579addceb9595583524d74562b3eed
Pablo Verdúguez <91903837+PabloVerduguez@users.noreply.github.com>
2024-10-19 20:39:13 +0200

    Refactorizar entidades CartaMenu e ItemMenu

---
+++ src/main/java/com/IsoII/DeliveringSolutions/persistencia/ItemMenuDAO.java
@@ -0,0 +1,10 @@
+package com.IsoII.DeliveringSolutions.persistencia;
+
+import org.springframework.stereotype.Repository;
+import com.IsoII.DeliveringSolutions.dominio.entidades.ItemMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface ItemMenuDAO extends JpaRepository<ItemMenu, String> {
+}
+
