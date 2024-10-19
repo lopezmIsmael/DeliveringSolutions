717f43e99dff8f86904f9cb117227fc2aa301216
lopezmIsmael <ismael.lopez6@alu.uclm.es>
2024-10-19 12:01:37 +0200

    Refactor GestorCliente.java and add verRestaurantes.html template
    - Refactor GestorCliente.java to include a new method "verRestaurantes" that retrieves a list of restaurants and adds them to the model.
    - Add verRestaurantes.html template to display the list of restaurants.

--- src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorCliente.java
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorCliente.java
@@ -1,27 +1,41 @@
 package com.IsoII.DeliveringSolutions.dominio.controladores;
 
-import com.IsoII.DeliveringSolutions.dominio.entidades.Cliente;
-import com.IsoII.DeliveringSolutions.persistencia.ClienteDAO;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
-import java.util.List;
+
+import com.IsoII.DeliveringSolutions.dominio.entidades.Cliente;
+import com.IsoII.DeliveringSolutions.dominio.entidades.Restaurante;
+import com.IsoII.DeliveringSolutions.persistencia.ClienteDAO;
+import com.IsoII.DeliveringSolutions.persistencia.RestauranteDAO;
 
 @Controller
 @RequestMapping("/clientes")
 public class GestorCliente {
+
     RedirectAttributes redirectAttributes = new RedirectAttributesModelMap();
 
     @Autowired
     private ClienteDAO clienteDAO;
 
+    @Autowired
+    private RestauranteDAO RestauranteDAO;
+
     // ************************************************** GETMAPPING
     // ********************************************** */
-
     // Método que devuelve una lista de todos los clientes
     @GetMapping("/findAll")
     @ResponseBody
@@ -40,6 +54,13 @@ public class GestorCliente {
     @ResponseBody
     public Cliente findById(@PathVariable String id) {
         return clienteDAO.findById(id).orElse(null);
+    }
+
+    @GetMapping("/verRestaurantes")
+    public String verRestaurantes(Model model) {
+        List<Restaurante> restaurantes = RestauranteDAO.findAll();
+        model.addAttribute("restaurantes", restaurantes);
+        return "verRestaurantes";
     }
 
     // ************************************************** POSTMAPPING
