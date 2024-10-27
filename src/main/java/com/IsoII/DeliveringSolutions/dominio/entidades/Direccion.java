510e8051ef61c2e91c97a471d9bed03a21e461d8
JorgeLopezGomez <Jorge.Lopez32@alu.uclm.es>
2024-10-27 19:11:17 +0100

    Refactorizar entidad Direccion y agregar getters y setters
    Co-Authored-By: lopezmIsmael <147510308+lopezmIsmael@users.noreply.github.com>

---
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/entidades/Direccion.java
@@ -0,0 +1,98 @@
+package com.IsoII.DeliveringSolutions.dominio.entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Direccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "calle", nullable = false, length = 50)
+    private String calle;
+
+    @Column(name = "numero", nullable = false, length = 5)
+    private String numero;
+
+    @Column(name = "complemento", length = 50)
+    private String complemento;
+
+    @Column(name = "municipio", nullable = false, length = 50)
+    private String municipio;
+
+    @OneToOne
+    @JoinColumn(name = "idCodigoPostal", referencedColumnName = "id", nullable = false)
+    private CodigoPostal codigoPostal;
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, String numero, String complemento, String municipio, CodigoPostal codigoPostal) {
+        this.calle = calle;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.municipio = municipio;
+        this.codigoPostal = codigoPostal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public CodigoPostal getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(CodigoPostal codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "id=" + id +
+                ", calle='" + calle + '\'' +
+                ", numero='" + numero + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", municipio='" + municipio + '\'' +
+                ", codigoPostal=" + codigoPostal +
+                '}';
+    }
+}
