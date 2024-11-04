0e83b2db03c0e1e0f45d6824f736b347d1434b4d
JorgeLopezGomez <Jorge.Lopez32@alu.uclm.es>
2024-11-04 17:25:15 +0100

    Añadido comentario descriptivo a las interfaces DAO para operaciones CRUD
    Co-Authored-By: marcowork2707 <147507132+marcowork2707@users.noreply.github.com>

--- src/main/java/com/IsoII/DeliveringSolutions/persistencia/PedidoDAO.java
+++ src/main/java/com/IsoII/DeliveringSolutions/persistencia/PedidoDAO.java
@@ -1,10 +1,10 @@
 package com.IsoII.DeliveringSolutions.persistencia;
 
-import org.springframework.stereotype.Repository;
 import com.IsoII.DeliveringSolutions.dominio.entidades.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+// Interfaz que extiende de JpaRepository para poder realizar operaciones CRUD sobre la entidad Pedido
 @Repository
-public interface PedidoDAO extends JpaRepository<Pedido, Integer> {
-   
+public interface PedidoDAO extends JpaRepository<Pedido, Integer> {  
 }
