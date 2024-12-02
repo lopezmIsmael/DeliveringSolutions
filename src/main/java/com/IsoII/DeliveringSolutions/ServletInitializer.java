a89edb5d2241a89ac1f48e28632f18cbcef4531e
Pablo Verdúguez <91903837+PabloVerduguez@users.noreply.github.com>
2024-12-02 19:46:13 +0100

    Eliminar importaciones no utilizadas en ServletInitializer

--- src/main/java/com/IsoII/DeliveringSolutions/ServletInitializer.java
+++ src/main/java/com/IsoII/DeliveringSolutions/ServletInitializer.java
@@ -2,8 +2,6 @@ package com.IsoII.DeliveringSolutions;
 
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @Controller
