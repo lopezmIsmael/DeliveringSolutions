00ca9d2610c18fe3101b1bfcf9d5ee95816211df
lopezmIsmael <ismael.lopez6@alu.uclm.es>
2024-12-02 20:03:44 +0100

    eliminacion import redundante

--- src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorPago.java
+++ src/main/java/com/IsoII/DeliveringSolutions/dominio/controladores/GestorPago.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import java.util.ArrayList;
 import java.util.List;
