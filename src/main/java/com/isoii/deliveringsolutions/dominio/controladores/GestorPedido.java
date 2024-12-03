f66d521f020b15a738f57986acb07d441333eafd
Pablo Verdúguez <91903837+PabloVerduguez@users.noreply.github.com>
2024-12-03 18:26:51 +0100

    Eliminar importación innecesaria de @RestController en GestorPedido.java

--- src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorPedido.java
+++ src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorPedido.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
