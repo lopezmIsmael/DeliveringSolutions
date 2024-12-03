e3a09f72641ffe4d390f6d5432cece2f000c5a2a
marcowork2707 <marco.munoz@alu.uclm.es>
2024-12-03 17:58:16 +0100

    Revert "Refactor controllers to use @RestController annotation and remove unnecessary @ResponseBody annotations"
    This reverts commit 7df1f59aea39ce5cbbc2aba80630294164998492.

--- src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorDireccion.java
+++ src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorDireccion.java
@@ -11,16 +11,16 @@ import com.isoii.deliveringsolutions.dominio.service.ServiceDireccion;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
 
 // Controlador para gestionar las direcciones
-@RestController
+@Controller
 @RequestMapping("/direccion")
 public class GestorDireccion {
 
@@ -90,12 +90,14 @@ public class GestorDireccion {
 
     // Método para encontrar una dirección por ID y mostrar su información
     @GetMapping("/findById/{id}")
+    @ResponseBody
     public Direccion findById(@PathVariable Long id) {
         return serviceDireccion.findById(id).orElse(null);
     }
 
     // Método para encontrar todas las direcciones y mostrarlas
     @GetMapping("/findAll")
+    @ResponseBody
     public List<Direccion> findAll() {
         return serviceDireccion.findAll();
     }
--- src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorPedido.java
+++ src/main/java/com/isoii/deliveringsolutions/dominio/controladores/GestorPedido.java
@@ -9,6 +9,7 @@ import com.isoii.deliveringsolutions.dominio.entidades.ItemPedido;
 import org.springframework.ui.Model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,10 +20,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.RestController;
-
 // Controlador para gestionar los pedidos
-@RestController
+@Controller
 @RequestMapping("/pedido")
 public class GestorPedido {
     RedirectAttributes redirectAttributes = new RedirectAttributesModelMap();
@@ -40,6 +39,7 @@ public class GestorPedido {
 
     // Método para mostrar todos los pedidos
     @GetMapping("/findAll")
+    @ResponseBody
     public List<Pedido> findAll() {
         return servicePedido.findAll();
     }
@@ -52,6 +52,7 @@ public class GestorPedido {
 
     // Método para buscar un pedido por su id
     @GetMapping("/findById/{id}")
+    @ResponseBody
     public Pedido findById(@PathVariable Integer id) {
         return servicePedido.findById(id).orElse(null);
     }
