3871270c2df8c00e94d8f47849d5540c6c62bbd8
marcowork2707 <marco.munoz@alu.uclm.es>
2024-12-10 17:57:19 +0100

    feat: add setter for id in Zona class

--- src/main/java/com/isoii/deliveringsolutions/dominio/entidades/Zona.java
+++ src/main/java/com/isoii/deliveringsolutions/dominio/entidades/Zona.java
@@ -38,6 +38,10 @@ public class Zona implements Serializable{
         return id;
     }    
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     // toString
     @Override
     public String toString() {
